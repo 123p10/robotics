39e2bf80160a3def8f9d87cfee0fd2ef106a9c3e
NickBombSquad <obrake3@gmail.com>
2017-06-04 11:42:48 -0400

    Video System
    Made a working video system and configured it for MySql

---
+++ upload.php
@@ -0,0 +1,56 @@
+<?php
+session_start();
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["fileName"]["name"]);
+$canUpload = 1;
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+if(isset($_POST["submit"])) {
+	if(file_exists($target_file)){
+		echo "It Already Exists";
+		$canUpload = 0;
+	}
+	if($canUpload == 0){
+		#header("location:videos.php");
+		echo "This file already exists IDIOT";
+	}
+	else{
+		if (move_uploaded_file($_FILES["fileName"]["tmp_name"], $target_file)) {
+			$servername = "127.0.0.1";
+			$username = "username";
+			$password = "password";
+			$dbname = "robot";
+			
+			$conn = new mysqli($servername, $username, $password, $dbname);
+			if ($conn->connect_error) {
+				die("Connection failed: " . $conn->connect_error);
+			}
+			$fn = $_POST['n'];
+			$sql = "INSERT INTO videos (name,url)
+			VALUES ('$fn','$target_file')";			
+			if ($conn->query($sql) === TRUE) {
+				echo "New record created successfully";
+			} else {
+				echo "Error: " . $sql . "<br>" . $conn->error;
+			}
+	
+			$conn->close();	
+			header("location:videos.php");
+		}
+		else{
+			if(is_writable("uploads/")){
+				echo "Directory is writable";
+			}
+			echo "WTF ARE YOU DOING STOP IT IDIOT OVER 50Mb file CHILLLLLLLLL with that";
+		#	header("location:videos.php");
+		}
+	}
+}
+
+
+function sqlUpload(){
+
+}
+?>
--- videos.php
+++ videos.php
@@ -2,6 +2,44 @@
 
 <html>
 <body>
+<span style="position:absolute;left:250px;top:200px;font-size:35px;">
+	<?php
+	$servername = "127.0.0.1";
+	$username = "username";
+	$password = "password";
+	$dbname = "robot";
+	
+	$conn = new mysqli($servername, $username, $password, $dbname);
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	}
+	$sql = "SELECT id, name, url FROM videos";
+	$result = $conn->query($sql);
+	if ($result->num_rows > 0) {
+		while($row = $result->fetch_assoc()) {
+			echo "<a href='" . $row["url"] . "'>" . $row["name"] . "</a>" . "<br>";
+		}
+	}
+	else {
+		echo "0 Videos Found.";
+	}
+	
+	
+	?>
+</span>
+
+
+
+<form method="post" action="upload.php" enctype="multipart/form-data" style="position:absolute;left:1100px;top:150px">
+    Select video to upload:
+    <input type="file" name="fileName" id="fileName">
+    <input type="submit" value="Upload Video" name="submit">
+	<br>
+	FileName: 
+	<input type="text" name="n" id="n">
+</form>
+
+
 
 </body>
 </html>
